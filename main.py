d77c7a37d52f0977edb87b559f45e1becc341cfb
kouki928 <furukawavo@gmail.com>
2023-12-13 00:22:44 +0900

    fix riku

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pickle
+import pandas as pd
 
 app = FastAPI()
 
@@ -15,35 +17,36 @@ class UserInfo(BaseModel):
     APreviousDayCompletion: float
     APreviousDayTarget: float
     AWeeklyCompletion: float
-    Age: str
+    Age: int
     Frequency: str
     Gender: str
     Goal: str
-    Height: str
+    Height: int
     LPreviousDayCompletion: float
     LPreviousDayTarget: float
     LWeeklyCompletion: float
     PPreviousDayCompletion: float
     PPreviousDayTarget: float
     PWeeklyCompletion: float
-    Weight: str
+    Weight: float
 
 def parsonSelection(data):
     # 目標BMIに基づく理想体重の算出
     goal_bmi = {
         "Male": {"MuscleStrength": 25.0, "WeightLoss": 22.0, "HealthMaintenance": 24.0},
-        "Female": {"MuscleStrength": 23.0, "WeightLoss": 20.0, "HealthMaintenance": 22.0}
+        "Female": {"MuscleStrength": 23.0, "WeightLoss": 20.0, "HealthMaintenance": 22.0},
+        "Other": {"MuscleStrength": 24.0, "WeightLoss": 21.0, "HealthMaintenance": 23.0}
     }
 
     # 辞書から必要な情報を取得
     Gender = data["Gender"]
     Frequency = data["Frequency"]
-    Age = int(data["Age"])
+    Age = data["Age"]
     Goal = data["Goal"]
-    Height = int(data["Height"])
-    Weight = float(data["Weight"])
+    Height = data["Height"]
+    Weight = data["Weight"]
 
-    # 目標BMIから理想体重を計算
+    # 目標BMIに基づく理想体重の算出
     ideal_bmi = goal_bmi[Gender][Goal]
     idealWeight = round(ideal_bmi * (Height / 100) ** 2, 1)
 
@@ -53,35 +56,37 @@ def parsonSelection(data):
     frequency_mapping = {"Low": 1, "Moderate": 2, "High": 3}
     Gender, Goal, Frequency = map(lambda x, mapping: mapping.get(x, x), [Gender, Goal, Frequency], [gender_mapping, goal_mapping, frequency_mapping])
 
-    # 部位ごとの目標回数を格納する辞書
-    TargetRepsDict = {}
-
     # A
-    APreviousDayCompletion = float(data["APreviousDayCompletion"])
-    AWeeklyCompletion = float(data["AWeeklyCompletion"])
-    APreviousDayTarget = float(data["APreviousDayTarget"])
+    APreviousDayCompletion = data["APreviousDayCompletion"]
+    AWeeklyCompletion = data["AWeeklyCompletion"]
+    APreviousDayTarget = data["APreviousDayTarget"]
     # L
-    LPreviousDayCompletion = float(data["LPreviousDayCompletion"])
-    LWeeklyCompletion = float(data["LWeeklyCompletion"])
-    LPreviousDayTarget = float(data["LPreviousDayTarget"])
+    LPreviousDayCompletion = data["LPreviousDayCompletion"]
+    LWeeklyCompletion = data["LWeeklyCompletion"]
+    LPreviousDayTarget = data["LPreviousDayTarget"]
     # P
-    PPreviousDayCompletion = float(data["PPreviousDayCompletion"])
-    PWeeklyCompletion = float(data["PWeeklyCompletion"])
-    PPreviousDayTarget = float(data["PPreviousDayTarget"])
+    PPreviousDayCompletion = data["PPreviousDayCompletion"]
+    PWeeklyCompletion = data["PWeeklyCompletion"]
+    PPreviousDayTarget = data["PPreviousDayTarget"]
 
-    # 新しいデータの構築
+    # MinMaxScalerのインスタンスを作成
+    scaler = MinMaxScaler()
+    # 選択した列のデータを正規化
     new_data = np.array([[Gender, Frequency, Age, Goal, Height, Weight, idealWeight, APreviousDayCompletion, AWeeklyCompletion, APreviousDayTarget, LPreviousDayCompletion, LWeeklyCompletion, LPreviousDayTarget, PPreviousDayCompletion, PWeeklyCompletion, PPreviousDayTarget]], dtype=np.float32)
 
+    # 特徴量の名前を指定してDataFrameを作成
+    columns = ["Gender", "Frequency", "Age", "Goal", "Height", "Weight", "IdealWeight", "APreviousDayCompletion", "AWeeklyCompletion", "APreviousDayTarget", "LPreviousDayCompletion", "LWeeklyCompletion", "LPreviousDayTarget", "PPreviousDayCompletion", "PWeeklyCompletion", "PPreviousDayTarget"]
+    df_data = pd.DataFrame(scaler.fit_transform(new_data), columns=columns)
+
     # 予測＆格納
-    keys = ["A", "L", "P"]
     TargetRepsDict = {}
     models = {"A": Amodel, "L": Lmodel, "P": Pmodel}
     category = {"A": "AbsTraining", "L": "LegTraining", "P": "PectoralTraining"}
     for model in models:
         # 推論
-        prediction = models[model].predict(new_data)[0]
+        prediction = models[model].predict(df_data)[0]
         # 推論結果を格納
-        TargetRepsDict[category[model]] = round(prediction, 1)
+        TargetRepsDict[category[model]] = round(prediction, 0)
 
     return TargetRepsDict
 
@@ -93,8 +98,3 @@ def bmi_prediction(req: UserInfo):
     result = parsonSelection(data)
     
     return result
-
-# if __name__ == "__main__":
-#     import uvicorn
-
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
