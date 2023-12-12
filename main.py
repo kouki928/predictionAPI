ea5aceb55dff5083b3192d4d5e70177f10da61ba
kouki928 <furukawavo@gmail.com>
2023-12-13 02:26:35 +0900

    add middleware and cors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # フロントエンドのオリジン
+    allow_origins=["*"],  # フロントエンドのオリジン
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
